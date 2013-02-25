1816db11337942f2347c091009749c517db37bed
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-02-25 20:53:54 +1100

    Deleting the existing config files to be replaced by links

--- 02-install-configs.sh
+++ 02-install-configs.sh
@@ -2,6 +2,15 @@
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# deleting the existing files
+rm -f ~/.zshrc
+rm -f ~/.zshenv
+rm -f ~/.gitconfig
+rm -f ~/.nanorc
+rm -f ~/.emacs
+rm -rf ~/.emacs.d
+rm -rf ~/.terminfo
+
 # zsh
 ln -s ${DIR}/home/.zshrc ~/.zshrc
 ln -s ${DIR}/home/.zshenv ~/.zshenv
